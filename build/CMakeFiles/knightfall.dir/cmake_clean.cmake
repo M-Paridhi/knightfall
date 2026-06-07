46456d991acbd4b7900f867426f649110cdc17ed
M-Paridhi <paridhimittal3106@gmail.com>
2026-06-07 10:52:02 +0530

    fixed captured piece history

---
+++ build/CMakeFiles/knightfall.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/knightfall.dir/src/core/attacks.cpp.obj"
+  "CMakeFiles/knightfall.dir/src/core/attacks.cpp.obj.d"
+  "CMakeFiles/knightfall.dir/src/core/bitboard.cpp.obj"
+  "CMakeFiles/knightfall.dir/src/core/bitboard.cpp.obj.d"
+  "CMakeFiles/knightfall.dir/src/core/board.cpp.obj"
+  "CMakeFiles/knightfall.dir/src/core/board.cpp.obj.d"
+  "CMakeFiles/knightfall.dir/src/core/movegen.cpp.obj"
+  "CMakeFiles/knightfall.dir/src/core/movegen.cpp.obj.d"
+  "CMakeFiles/knightfall.dir/src/main.cpp.obj"
+  "CMakeFiles/knightfall.dir/src/main.cpp.obj.d"
+  "knightfall.exe"
+  "knightfall.exe.manifest"
+  "knightfall.pdb"
+  "libknightfall.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/knightfall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/knightfall.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for knightfall.
+# This may be replaced when dependencies are built.
